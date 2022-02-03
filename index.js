f88ed7a7fb44b961e32af963f20331cb25c3f144
Ashley Xu <amx8198@ads.northwestern.edu>
2022-02-02 23:39:23 -0600

    Trying trustprotoheader.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require('path');
 const http = require('http');
 const enforce = require('express-sslify');
 
+// Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
+// a load balancer (e.g. Heroku). See further comments below
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
